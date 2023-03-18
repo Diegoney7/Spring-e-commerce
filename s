b7314e1577d7e6f6b052e01986e3a84cfd81ed20
Diegoney7 <ney.flores.c@uni.pe>
2023-03-18 09:59:52 -0500

    creacion de clases model y mapeo jpa a tablas e integracion de diseño y vistas

---
+++ s
@@ -0,0 +1,11 @@
+[33mcommit a6967e100a3aaded0c636c3d0565936ee914c8e6[m[33m ([m[1;36mHEAD -> [m[1;32mmaster[m[33m, [m[1;31morigin/master[m[33m)[m
+Author: Diegoney7 <128003534+Diegoney7@users.noreply.github.com>
+Date:   Thu Mar 16 12:02:41 2023 -0500
+
+    Create README.md
+
+[33mcommit 812d4adfae5f86e0ab67899cc5fc1ed51f130bf5[m
+Author: Diegoney7 <ney.flores.c@uni.pe>
+Date:   Thu Mar 16 11:45:17 2023 -0500
+
+    Commit proyecto Spring-ecommerce
--- src/main/java/com/curso/ecommerce/SpringEcommerceApplication.java
+++ src/main/java/com/curso/ecommerce/SpringEcommerceApplication.java
@@ -2,9 +2,8 @@ package com.curso.ecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication (exclude = DataSourceAutoConfiguration.class)
+@SpringBootApplication
 public class SpringEcommerceApplication {
 
 	public static void main(String[] args) {
@@ -12,3 +11,4 @@ public class SpringEcommerceApplication {
 	}
 
 }
+ 	
